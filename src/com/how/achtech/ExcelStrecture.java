1baa4fd5b0488f5509411b9ec88878d3df1e1df3
achraf saloumi <a.mareshal@gmail.com>
2018-07-24 21:00:50 +0100

    second

---
+++ src/com/how/achtech/ExcelStrecture.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.how.achtech;
+
+/**
+ * @author Vous
+ *
+ */
+public class ExcelStrecture {
+	private String wikihowUrl;
+	private String titre;
+	private String packageName;
+	private String logoUrl;
+	private String backgroundUrl;
+	private String description;
+	private String keyword;
+	public String getWikihowUrl() {
+		return wikihowUrl;
+	}
+	public void setWikihowUrl(String wikihowUrl) {
+		this.wikihowUrl = wikihowUrl;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getPackageName() {
+		return packageName;
+	}
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+	public void setLogoUrl(String logoUrl) {
+		this.logoUrl = logoUrl;
+	}
+	public String getBackgroundUrl() {
+		return backgroundUrl;
+	}
+	public void setBackgroundUrl(String backgroundUrl) {
+		this.backgroundUrl = backgroundUrl;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	
+}
